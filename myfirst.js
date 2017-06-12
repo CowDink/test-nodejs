d9338569a61eb75225d527e96a594c8e0b38696d
DooDink <sattanationmail@gmail.com>
2017-06-12 18:00:00 +0700

    fixing some bugs

--- myfirst.js
+++ myfirst.js
@@ -6,15 +6,18 @@ var fs = require('fs');
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
     var filename = "."+q.pathname;
-    fs.readFile(filename, function (err, data){
-		if(err){
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
-		}
-		res.writeHead(200, {'Content-type': 'text/html'});
-		res.write(data);
-		return res.end();
-	});
+   // res.write(filename);
+    if(filename){
+   	 fs.readFile(filename+".html", function (err, data){
+			if(err){
+				res.writeHead(404, {'Content-Type': 'text/html'});
+				return res.end("404 Not Found");
+			}
+			res.writeHead(200, {'Content-type': 'text/html'});
+			res.write(filename+" "+data);
+			return res.end();
+		});
+	}
 }).listen(8080);
 
 
