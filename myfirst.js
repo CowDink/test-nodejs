9cafeea44679e54e10f4cc01de0b8f450d68b728
DooDink <sattanationmail@gmail.com>
2017-06-12 13:41:10 +0700

    Try some fs things

--- myfirst.js
+++ myfirst.js
@@ -1,1 +1,20 @@
-console.log('This is Node.js running on Android.')
+var http = require('http');
+var url = require('url');
+var waktu = require('./module_waktu');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	var q = url.parse(req.url, true);
+    var filename = "."+q.pathname;
+    fs.readFile(filename, function (err, data){
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404 Not Found");
+		}
+		res.writeHead(200, {'Content-type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+}).listen(8080);
+
+
